36c068b95d5e3cf2fddff6c371442cab5d9f0266
agneshew <agnes.hewusz@gmail.com>
2021-07-01 21:23:19 +0200

    fixed bugs in CyclicRotation

--- src/com/agnes/CyclicRotation.java
+++ src/com/agnes/CyclicRotation.java
@@ -5,13 +5,19 @@ public class CyclicRotation {
     public void solution(int[] A, int K) {
 
         int[] arr = new int[A.length];
+        System.arraycopy(A, 0, arr, 0, A.length);
 
-        for (int i = 0; i < K; i++) {
-            arr[0] = A[(A.length - 1)];
-            for (int j = 0; j < (A.length - 1); j++) {
-                arr[j+1] = A[(j)];
+            for (int i = 0; i < K; i++) {
+                int[] arrTemp = new int[arr.length];
+                arrTemp[0] = arr[(arr.length - 1)];
+
+                for (int j = 0; j < (arr.length - 1); j++) {
+                    arrTemp[j + 1] = arr[(j)];
+                }
+                System.arraycopy(arrTemp, 0, arr, 0, arr.length);
+
             }
-        }
+
         for (int i=0; i < arr.length ; i++) {
             System.out.print(arr[i] + " ");
         }
--- src/com/agnes/Main.java
+++ src/com/agnes/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         BinaryGaps binaryGaps = new BinaryGaps();
-        binaryGaps.solution(168);
+        binaryGaps.solution(1041);
 
-        int K = 2;
+        int K = 3;
         int[] arr = new int[]{2, 3, 5, 6, 8, 9};
         for (int i=0; i < arr.length ; i++) {
             System.out.print(arr[i] + " ");
